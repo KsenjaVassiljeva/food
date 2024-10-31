fca17a886d16eb7c13594608fc631f2dd66b7baf
KsenjaVassiljeva <145933696+KsenjaVassiljeva@users.noreply.github.com>
2024-10-31 13:25:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
-import cards from './modules/cards';
+import cards from  './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
-import {openModal} from './modules/modal';
+import {openModal}  from './modules/modal';
 
-window.addEventListener('DOMContentLoaded', function(){
-    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 5000);
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
 
-    tabs('.tabheader_item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-    modal('[data-modal]', modalTimerId);
-    timer('.timer', '2025-10-31');
+    tabs('.tableheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-10-31');
     cards();
     calc();
     forms('form', modalTimerId);
@@ -24,6 +24,6 @@ window.addEventListener('DOMContentLoaded', function(){
         totalCounter: '#total',
         currentCounter: '#current',
         wrapper: '.offer__slider-wrapper',
-        field: '.offer__slider-inner',
+        field: '.offer__slider-inner'
     });
 });
